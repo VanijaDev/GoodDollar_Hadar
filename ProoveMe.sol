d8ca76a5c9ae708801d36a419dcb5eedbbc80140
Vanija <vanijatko.social@gmail.com>
2020-11-09 19:55:25 +0200

    Update ProoveMe.sol
    Front run defence

--- ProoveMe.sol
+++ ProoveMe.sol
@@ -2,19 +2,33 @@
 
 pragma solidity ^0.7.4;
 
-contract ProoveMe {
+contract ProveMe {
     mapping (address => address) public provedOwnerForOwned;
+    mapping (address => bytes32) public messageForOwner; // should be signed by owned address, but send by owner as prove of owned address ownership
+    mapping (address => bytes32) private messageForOwnerHashed;
     
     event Owner(address owner, address owned);
     
+    /**
+      *@dev Used for front run resistance. Each address should have individual messgae to be hashed by owned address.
+    */
+    function register() external {
+        require(messageForOwner[msg.sender] == 0, "Already registered");
+        
+        bytes32 byteAddress = bytes32(uint256(msg.sender) << 96);
+        messageForOwner[msg.sender] = byteAddress;
+        
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        messageForOwnerHashed[msg.sender] = keccak256(abi.encodePacked(prefix, byteAddress));
+    }
     
-    
-    function prooveMe(address _ownedAddr, bytes32 _messageHash, bytes memory _signature) external {
+    function proveMe(address _ownedAddr, bytes memory _signature) external {
         require(provedOwnerForOwned[_ownedAddr] == address(0), "owner present");
+        require(bytes32(uint256(msg.sender) << 96) == messageForOwner[msg.sender], "wrong sender");
         
         (uint8 v, bytes32 r, bytes32 s) = splitSignature(_signature);
 
-        address signerAddr = ecrecover(_messageHash, v, r, s);
+        address signerAddr = ecrecover(messageForOwnerHashed[msg.sender], v, r, s);
         require(signerAddr == _ownedAddr, "Not owner");
         
         provedOwnerForOwned[_ownedAddr] = msg.sender;
